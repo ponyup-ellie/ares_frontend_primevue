83b211d5309e253c0428a3639d71f54b05c70033
sooyoonhan <ponyup.ellie@gmail.com>
2021-05-16 15:21:39 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import { createApp } from 'vue'
+import App from './App.vue';
+import router from './router'
+
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.use(ToastService);
+app.use(router);
+
+app.component('InputText', InputText);
+app.component('Button', Button);
+app.component('Toast', Toast);
+
+app.mount('#app')
